16ea8117c8ef96462f6140b0c8be8b847a734d1a
alex irazabal <alexirazabal@alexs-mbp-2.lan>
2021-08-24 11:36:25 -0400

    changed to SASL_MECHANISM

--- src/main/java/io/confluent/developer/MergeStreams.java
+++ src/main/java/io/confluent/developer/MergeStreams.java
@@ -93,6 +93,10 @@ public class MergeStreams {
         allProps.put(SaslConfigs.SASL_JAAS_CONFIG,allProps.getProperty("sasl.jaas.config"));
         allProps.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, allProps.getProperty("ssl.truststore.password.config"));
         
+        //allProps.put(SCHEMA_REGISTRY_SSL_TRUSTSTORE_LOCATION, allProps.getProperty("schema.registry.ssl.truststore.location"));
+        //allProps.put(SCHEMA_REGISTRY_SSL_TRUSTSTORE_PASSWORD, allProps.getProperty("schema.registry.ssl.truststore.password"));
+
+
         Topology topology = ms.buildTopology(allProps);
 
         //ms.createTopics(allProps);
